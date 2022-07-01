27c93235751b0cbc071816eed5046400d7ced7e1
Bluzzi <camilledugas.pro@gmail.com>
2022-07-01 21:04:41 +0200

    index: test

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,8 @@ import Anchor from "../lib/components/elements/text/Anchor";
 import Text from "../lib/components/elements/text/Text";
 import Navbar from "../lib/components/layouts/navbar/Navbar";
 import Particles from "../lib/components/specials/Particles";
+import Image from "next/image";
+import CircleMenu from "../lib/components/elements/CircleMenu";
 
 export default function Home() : ReactElement {
   return (
@@ -20,7 +22,7 @@ export default function Home() : ReactElement {
             <Text type="h1" color="gray" size="3xl">a Web Developer.</Text>
 
             <div className="w-1/2">
-              <Text color="gray" size="large">I{"'"}m a passionate full-stack web developer since 2016. I mainly work with the technologies proposed by the NodeJS universe to create modern web applications.</Text>
+              <Text color="gray" size="large" type="h2">I{"'"}m a passionate full-stack web developer since 2016. I mainly work with the technologies proposed by the NodeJS universe to create modern web applications.</Text>
             </div>
 
             <Button href="/#about" large>Discover me</Button>
@@ -28,9 +30,23 @@ export default function Home() : ReactElement {
         </div>
       </header>
 
-      <section id="about" className="h-screen bg-about flex items-center justify-center">
-        <Text>Lorem ipsum dolor sit amet <Anchor href="#">consectetur adipisicing</Anchor> elit. Veniam autem perferendis iure cupiditate dolore tenetur molestias iusto quos deserunt facere. Quo aliquam ducimus laudantium minima asperiores ab possimus, incidunt veritatis.</Text>
+      <section id="about" className="h-screen bg-about flex items-center justify-center flex-col">
+        <Text size="3xl" type="h2">Who I am ?</Text>
+
+        <div className="w-3/4 flex">
+          <div>
+            <Text>Lorem ipsum dolor sit amet <Anchor href="#">consectetur adipisicing</Anchor> elit. Veniam autem perferendis iure cupiditate dolore tenetur molestias iusto quos deserunt facere. Quo aliquam ducimus laudantium minima asperiores ab possimus, incidunt veritatis.</Text>
+
+            <Text>Lorem ipsum dolor sit amet <Anchor href="#">consectetur adipisicing</Anchor> elit. Veniam autem perferendis iure cupiditate dolore tenetur molestias iusto quos deserunt facere. Quo aliquam ducimus laudantium minima asperiores ab possimus, incidunt veritatis.</Text>
+
+            <Text>Lorem ipsum dolor sit amet <Anchor href="#">consectetur adipisicing</Anchor> elit. Veniam autem perferendis iure cupiditate dolore tenetur molestias iusto quos deserunt facere. Quo aliquam ducimus laudantium minima asperiores ab possimus, incidunt veritatis.</Text>
+          </div>
+
+          <Image src="/images/profile-picture.jpg" alt="profile picture" height={400} width={400} className="rounded-full" />
+        </div>
       </section>
+
+      <CircleMenu/>
     </main>
   );
 }
