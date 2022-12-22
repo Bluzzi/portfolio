078192032ca9ec960f0e22fd62a15287e014ea56
Bluzzi <camilledugas.pro@gmail.com>
2022-12-22 11:27:38 +0100

    Under development...

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,14 @@ import { ReactElement } from "react";
 
 export default function Home(): ReactElement {
   return (
-    <div className="grid place-content-center h-screen">
-      <p className="text-white">Hello world!</p>
+    <div className="grid place-content-center gap-2 h-screen">
+      <p className="text-white">Mon portfolio est en création...</p>
 
-      <LinkButton href="https://twitter.com/Bluzzi">About me</LinkButton>
+      <div className="flex gap-3 flex-wrap">
+        <LinkButton href="https://github.com/Bluzzi">GitHub</LinkButton>
+        <LinkButton href="https://www.malt.fr/profile/camilledugas">Malt</LinkButton>
+        <LinkButton href="https://www.linkedin.com/in/camille-dugas/">LinkedIn</LinkButton>
+      </div>
     </div>
   );
 }
