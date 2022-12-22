dada6a6820d22b5932a02870a807311288090b62
Bluzzi <camilledugas.pro@gmail.com>
2022-12-22 10:56:19 +0100

    /layout (app): add scroll smooth and black background

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import { PropsWithChildren, ReactElement } from "react";
 import "$lib/styles/tailwind.css";
 import "$lib/styles/reset.css";
-import "$lib/styles/global.css";
 import "$lib/styles/font.css";
 
 export default function Layout({ children }: PropsWithChildren): ReactElement {
@@ -9,7 +8,9 @@ export default function Layout({ children }: PropsWithChildren): ReactElement {
     <html>
       <head />
 
-      <body>{children}</body>
+      <body className="scroll-smooth bg-black">
+        {children}
+      </body>
     </html>
   );
 }
