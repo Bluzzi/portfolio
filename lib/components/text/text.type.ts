941bbb6a4b4f9e95ca733ddc52e7e8696eb00165
Bluzzi <camilledugas.pro@gmail.com>
2022-12-23 03:54:43 +0100

    text (component/type): change use the right interface for paragraph

--- lib/components/text/text.type.ts
+++ lib/components/text/text.type.ts
@@ -1,6 +1,6 @@
-import { ParamHTMLAttributes, PropsWithChildren } from "react";
+import { HTMLAttributes, PropsWithChildren } from "react";
 
-export type TextProps = PropsWithChildren & ParamHTMLAttributes<HTMLParagraphElement> & {
+export type TextProps = PropsWithChildren & HTMLAttributes<HTMLParagraphElement> & {
   type?: "p" | "h3" | "h2" | "h1";
   color?: "white" | "black" | "gray";
   size?: "base" | "large" | "xl" | "2xl" | "3xl";
