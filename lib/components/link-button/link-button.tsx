2c691c8382fc173f057ba4f54d3152beee274a9c
Bluzzi <camilledugas.pro@gmail.com>
2022-12-23 13:55:27 +0100

    link-button (component): change font color

--- lib/components/link-button/link-button.tsx
+++ lib/components/link-button/link-button.tsx
@@ -2,5 +2,5 @@ import { ReactElement } from "react";
 import { LinkButtonProps } from "./link-button.type";
 
 export function LinkButton({ children, ...props }: LinkButtonProps): ReactElement {
-  return <a className="text-gray py-1 px-5 rounded border border-gray hover:text-white hover:border-white" {...props}>{children}</a>;
+  return <a className="text-white py-1 px-5 rounded border border-gray hover:border-white" {...props}>{children}</a>;
 }
