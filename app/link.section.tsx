5155273a2d279b569587022ccc347f7b722acc29
Bluzzi <camilledugas.pro@gmail.com>
2022-12-23 17:09:27 +0100

    / (page): add email in link section

--- app/link.section.tsx
+++ app/link.section.tsx
@@ -7,6 +7,7 @@ export function LinkSection(): ReactElement {
       <LinkButton href="https://github.com/Bluzzi">GitHub</LinkButton>
       <LinkButton href="https://www.linkedin.com/in/camille-dugas">LinkedIn</LinkButton>
       <LinkButton href="https://www.malt.fr/profile/camilledugas">Malt</LinkButton>
+      <LinkButton href="mailto:camilledugas.pro@gmail.com">Email</LinkButton>
     </div>
   );
 }
