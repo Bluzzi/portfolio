b42a6d8ea00c3ab4252cacf221f2bd3da6b74d81
Bluzzi <camilledugas.pro@gmail.com>
2022-12-23 13:00:13 +0100

    Split skill and experience into a JSON config

--- app/experience-section.tsx
+++ app/experience-section.tsx
@@ -1,10 +1,17 @@
 import { Text } from "$lib/components/text";
 import { ReactElement } from "react";
+import experiences from "$lib/configs/experience.json";
 
 export function ExperienceSection(): ReactElement {
   return (
     <div>
       <Text size="2xl">Mes expériences professionnelles</Text>
+
+      {experiences.map((experience, index) => {
+        return (
+          <Text key={index}>{experience.title}</Text>
+        );
+      })}
     </div>
   );
 }
