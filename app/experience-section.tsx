404f16410ac1296b8d46e324df89693b9cd56f35
Bluzzi <camilledugas.pro@gmail.com>
2022-12-23 13:52:08 +0100

    / (page): work in progress...

--- app/experience-section.tsx
+++ app/experience-section.tsx
@@ -1,15 +1,37 @@
 import { Text } from "$lib/components/text";
 import { ReactElement } from "react";
-import experiences from "$lib/configs/experience.json";
+import experienceConfig from "$lib/configs/experience.json";
+import dayjs from "dayjs";
 
 export function ExperienceSection(): ReactElement {
+  // Parse experiences dates:
+  const experiences = experienceConfig.map(experience => ({
+    ...experience,
+    date: {
+      start: dayjs(experience.date.start),
+      end: experience.date.end ? dayjs(experience.date.end) : null
+    }
+  }));
+
+  // Sort experiences by start date:
+  experiences.sort((a, b) => a.date.start.isAfter(b.date.start) ? 1 : -1);
+
+  // Component content:
   return (
     <div>
       <Text size="2xl">Mes expériences professionnelles</Text>
 
       {experiences.map((experience, index) => {
         return (
-          <Text key={index}>{experience.title}</Text>
+          <div key={index} className="my-3">
+            <Text className="mb-1">
+              {experience.date.start.format("DD/MM/YYYY")} - {experience.date.end ? experience.date.end.format("DD/MM/YYYY") : "présent"} (5 mois)
+            </Text>
+
+            <Text>{experience.company.toUpperCase()} - {experience.title}</Text>
+
+            {experience.description.map(line => <Text key={line} color="gray" size="small" className="ml-1">● {line}</Text>)}
+          </div>
         );
       })}
     </div>
--- app/skill-section.tsx
+++ app/skill-section.tsx
@@ -1,11 +1,11 @@
 import { ReactElement } from "react";
 import { Skill } from "$lib/components/skill";
-import skills from "$lib/configs/skill.json";
+import skillConfig from "$lib/configs/skill.json";
 
 export function SkillSection(): ReactElement {
   return (
     <div className="flex flex-wrap gap-2">
-      {skills.map(skill => <Skill key={skill.title} title={skill.title} logoSrc={skill.logo} link={skill.link} />)}
+      {skillConfig.map(skill => <Skill key={skill.title} title={skill.title} logoSrc={skill.logo} link={skill.link} />)}
     </div>
   );
 }
