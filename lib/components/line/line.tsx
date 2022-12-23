6e0fe1876c9eaeb89e4f76a411b4f844d2bb23d3
Bluzzi <camilledugas.pro@gmail.com>
2022-12-23 12:44:27 +0100

    line (component): display block

--- lib/components/line/line.tsx
+++ lib/components/line/line.tsx
@@ -3,5 +3,5 @@ import { LineProps } from "./line.type";
 import { clsx } from "clsx";
 
 export function Line({ className, ...props }: LineProps): ReactElement {
-  return <span className={clsx(className, "bg-gray h-[1px] w-full")} {...props} />;
+  return <span className={clsx(className, "block bg-gray h-[1px] w-full")} {...props} />;
 }
