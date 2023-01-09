f26713160bf9b90dd032a9f3036ba3847318877e
Bluzzi <camilledugas.pro@gmail.com>
2023-01-09 11:44:28 +0100

    Add Vanilla Extract skill

--- app/link.section.tsx
+++ app/link.section.tsx
@@ -4,10 +4,10 @@ import { ReactElement } from "react";
 export function LinkSection(): ReactElement {
   return (
     <div className="flex gap-2 flex-wrap">
-      <LinkButton href="https://github.com/Bluzzi">GitHub</LinkButton>
-      <LinkButton href="https://www.linkedin.com/in/camille-dugas">LinkedIn</LinkButton>
-      <LinkButton href="https://www.malt.fr/profile/camilledugas">Malt</LinkButton>
-      <LinkButton href="mailto:camilledugas.pro@gmail.com">Email</LinkButton>
+      <LinkButton href="https://github.com/Bluzzi" target="_blank">GitHub</LinkButton>
+      <LinkButton href="https://www.linkedin.com/in/camille-dugas" target="_blank">LinkedIn</LinkButton>
+      <LinkButton href="https://www.malt.fr/profile/camilledugas" target="_blank">Malt</LinkButton>
+      <LinkButton href="mailto:camilledugas.pro@gmail.com" target="_blank">Email</LinkButton>
     </div>
   );
 }
