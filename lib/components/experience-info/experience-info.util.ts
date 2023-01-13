8c63249ca0abfb50e518adacabfcc351ba8669d1
Bluzzi <camilledugas.pro@gmail.com>
2023-01-13 02:27:33 +0100

    experience-info (component): fix color of open source experience type

--- lib/components/experience-info/experience-info.util.ts
+++ lib/components/experience-info/experience-info.util.ts
@@ -4,7 +4,7 @@ import { ExperienceTypeLabelInfo } from "./experience-info.type";
 export function getLabelInfoByExperienceType(type: ExperienceType): ExperienceTypeLabelInfo {
   switch (type) {
     case "open-source": {
-      return { color: "blue", text: "open source" };
+      return { color: "green", text: "open source" };
     }
 
     case "pro": {
