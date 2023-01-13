0118e3728c6c3614469d8e73b14fb285f184b484
Bluzzi <camilledugas.pro@gmail.com>
2023-01-13 20:58:24 +0100

    text (component): syntax

--- lib/components/text/text.tsx
+++ lib/components/text/text.tsx
@@ -4,12 +4,12 @@ import { TextProps } from "./text.type";
 
 export function Text({ children, type, color, size, className, ...props }: TextProps): ReactElement {
   const style = clsx(className, {
-    // Text color :
+    // Text color:
     "text-white": !color || color === "white",
     "text-black": color === "black",
     "text-gray": color === "gray",
 
-    // Font size :
+    // Font size:
     "text-sm": size === "small",
     "text-base": !size || size === "base",
     "text-lg": size === "large",
