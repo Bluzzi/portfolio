7411346d371f6467f058103b07cabdec4343e729
Bluzzi <camilledugas.pro@gmail.com>
2023-02-02 18:53:53 +0100

    / (page, layout): move main div style in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,9 @@ export default function Layout({ children }: PropsWithChildren): ReactElement {
       <head />
 
       <body className="scroll-smooth bg-black min-h-screen py-16">
-        {children}
+        <main className="mx-auto w-1/2 lg:w-3/4 sm:w-11/12">
+          {children}
+        </main>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -8,18 +8,16 @@ import { LinkSection } from "./link.section";
 export default function Home(): ReactElement {
   return (
     <>
-      <div className="mx-auto w-1/2 lg:w-3/4 sm:w-11/12">
-        <AboutSection />
-        <Line className="my-7" />
+      <AboutSection />
+      <Line className="my-7" />
 
-        <SkillSection />
-        <Line className="my-7" />
+      <SkillSection />
+      <Line className="my-7" />
 
-        <ExperienceSection />
-        <Line className="my-7" />
+      <ExperienceSection />
+      <Line className="my-7" />
 
-        <LinkSection />
-      </div>
+      <LinkSection />
     </>
   );
 }
