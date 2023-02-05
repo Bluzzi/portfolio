7997772bd2762468030fde0307951be9f4521317
Bluzzi <camilledugas.pro@gmail.com>
2023-02-05 02:35:50 +0100

    skill-button (component): add unit tests (temporary disabled)

---
+++ lib/components/skill-button/skill-button.test.tsx
@@ -0,0 +1,26 @@
+import { test, expect, describe } from "vitest";
+import { render, screen } from "@testing-library/react";
+import { SkillButton } from "./skill-button";
+
+describe("SkillButton", () => {
+  // Waiting solution for NextJS <Image /> component
+
+  // test("display the text", () => {
+  //   render(<SkillButton title="React" link="https://example.com" logoSrc="/skills/react.png" />);
+
+  //   expect(screen.getByText("React")).toBeDefined();
+  // });
+
+  // test("link and target", () => {
+  //   render(<SkillButton title="React" link="https://example.com" logoSrc="/skills/react.png" />);
+
+  //   expect(screen.getByRole("link").getAttribute("href")).toBe("https://example.com");
+  //   expect(screen.getByRole("link").getAttribute("target")).toBe("_blank");
+  // });
+
+  // test("render the icon", () => {
+  //   render(<SkillButton title="React" link="https://example.com" logoSrc="/skills/react.png" />);
+
+  //   expect(screen.getByRole("img")).toBeDefined();
+  // });
+});
