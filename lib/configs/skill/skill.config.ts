27dd8f411bff78c6d53c38ac5eee9faaffa266cd
Bluzzi <camilledugas.pro@gmail.com>
2023-02-10 23:07:57 +0100

    skill (config): divers changes

--- lib/configs/skill/skill.config.ts
+++ lib/configs/skill/skill.config.ts
@@ -5,12 +5,14 @@ export const skills: Skill[] = [
   { title: "React", logo: "/skills/react.png", link: "https://fr.reactjs.org" },
   { title: "Next", logo: "/skills/next-js.svg", link: "https://nextjs.org" },
   // { title: "CSS", logo: "/skills/css.png", link: "https://developer.mozilla.org/fr/docs/Web/CSS" },
-  { title: "SASS", logo: "/skills/sass.png", link: "https://sass-lang.com" },
+  // { title: "SASS", logo: "/skills/sass.png", link: "https://sass-lang.com" },
   { title: "Tailwind", logo: "/skills/tailwind.svg", link: "https://tailwindcss.com" },
   { title: "Vanilla Extract", logo: "/skills/vanilla-extract.svg", link: "https://vanilla-extract.style" },
-  { title: "Mantine", logo: "/skills/mantine.svg", link: "https://mantine.dev" },
+  // { title: "Mantine", logo: "/skills/mantine.svg", link: "https://mantine.dev" },
+  // { title: "NextUI", logo: "/skills/next-ui.svg", link: "https://nextui.org" },
   { title: "Radix", logo: "/skills/radix.svg", link: "https://www.radix-ui.com" },
   { title: "Zustand", logo: "/skills/zustand.png", link: "https://zustand-demo.pmnd.rs" },
+  { title: "Zod", logo: "/skills/zod.svg", link: "https://zod.dev" },
   { title: "Vitest", logo: "/skills/vitest.svg", link: "https://vitest.dev" },
   { title: "Testing Library", logo: "/skills/testing-library.png", link: "https://testing-library.com" },
   { title: "Playwright", logo: "/skills/playwright.svg", link: "https://playwright.dev" },
@@ -18,7 +20,6 @@ export const skills: Skill[] = [
   { title: "React Query", logo: "/skills/react-query.png", link: "https://tanstack.com/query/v4" },
   { title: "React Hook Form", logo: "/skills/react-hook-form.svg", link: "https://react-hook-form.com" },
   { title: "React Email", logo: "/skills/react-email.svg", link: "https://react.email" },
-  { title: "Zod", logo: "/skills/zod.svg", link: "https://zod.dev" },
-  { title: "DayJS", logo: "/skills/dayjs.png", link: "https://day.js.org" },
-  { title: "GraphQL", logo: "/skills/graphql.png", link: "https://graphql.org" }
+  { title: "DayJS", logo: "/skills/dayjs-emoji.png", link: "https://day.js.org" }
+  // { title: "GraphQL", logo: "/skills/graphql.png", link: "https://graphql.org" }
 ];
