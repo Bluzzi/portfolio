afdb708443704748fa80557f995c6f807c1ded4c
Bluzzi <camilledugas.pro@gmail.com>
2023-02-12 01:30:27 +0100

    / (page): change my contact email

--- app/link.section.tsx
+++ app/link.section.tsx
@@ -7,7 +7,7 @@ export function LinkSection(): ReactElement {
       <LinkButton href="https://github.com/Bluzzi" target="_blank">GitHub</LinkButton>
       <LinkButton href="https://www.linkedin.com/in/camille-dugas" target="_blank">LinkedIn</LinkButton>
       <LinkButton href="https://www.malt.fr/profile/camilledugas" target="_blank">Malt</LinkButton>
-      <LinkButton href="mailto:camilledugas.pro@gmail.com" target="_blank">Email</LinkButton>
+      <LinkButton href="mailto:contact@camilledugas.me" target="_blank">Email</LinkButton>
     </div>
   );
 }
