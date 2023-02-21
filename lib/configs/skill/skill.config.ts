ac6354f313ad8bdd4b1b3117f50bf63a1afa356e
Bluzzi <camilledugas.pro@gmail.com>
2023-02-21 23:43:35 +0100

    skill (config): add eslint

--- lib/configs/skill/skill.config.ts
+++ lib/configs/skill/skill.config.ts
@@ -4,7 +4,7 @@ export const skills: Skill[] = [
   { title: "TypeScript", logo: "/skills/typescript.svg", link: "https://www.typescriptlang.org" },
   { title: "React", logo: "/skills/react.png", link: "https://fr.reactjs.org" },
   { title: "Next", logo: "/skills/next-js.svg", link: "https://nextjs.org" },
-  // { title: "CSS", logo: "/skills/css.png", link: "https://developer.mozilla.org/fr/docs/Web/CSS" },
+  // { title: "CSS", logo: "/skills/css.png", link: "https://github.com/css-modules/css-modules },
   // { title: "SASS", logo: "/skills/sass.png", link: "https://sass-lang.com" },
   { title: "Tailwind", logo: "/skills/tailwind.svg", link: "https://tailwindcss.com" },
   // { title: "Vanilla Extract", logo: "/skills/vanilla-extract.svg", link: "https://vanilla-extract.style" },
@@ -20,6 +20,7 @@ export const skills: Skill[] = [
   { title: "React Query", logo: "/skills/react-query.png", link: "https://tanstack.com/query/v4" },
   { title: "React Hook Form", logo: "/skills/react-hook-form.svg", link: "https://react-hook-form.com" },
   { title: "React Email", logo: "/skills/react-email.svg", link: "https://react.email" },
-  { title: "DayJS", logo: "/skills/dayjs-emoji.png", link: "https://day.js.org" }
+  { title: "DayJS", logo: "/skills/dayjs-emoji.png", link: "https://day.js.org" },
+  { title: "ESLint", logo: "/skills/eslint.png", link: "https://eslint.org/" }
   // { title: "GraphQL", logo: "/skills/graphql.png", link: "https://graphql.org" }
 ];
