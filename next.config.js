89611b91c1fec68b14d5f512576086af6b67d628
Bluzzi <camilledugas.pro@gmail.com>
2023-02-26 19:24:20 +0100

    next (config): add Typewind SWC plugin

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
-/** @type {import('next').NextConfig} */
+/** @type {import("next").NextConfig} */
 const nextConfig = {
   experimental: {
-    appDir: true
+    appDir: true,
+    swcPlugins: [["typewind/swc", {}]]
   }
 };
 
