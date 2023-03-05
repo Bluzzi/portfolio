75560fe2b86b36de83b160ff70efee730e30d5bc
Bluzzi <camilledugas.pro@gmail.com>
2023-03-05 03:22:28 +0100

    / (layout): use the new NextJS metadata API

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,14 @@
-import { PropsWithChildren, ReactElement } from "react";
+import type { PropsWithChildren, ReactElement } from "react";
 import "$lib/styles/tailwind.css";
 import "$lib/styles/reset.css";
 import "$lib/styles/font.css";
+import { metadata } from "$lib/configs/metadata";
 import { tw } from "typewind";
 
+// Metadata :
+export { metadata };
+
+// Layout:
 export default function Layout({ children }: PropsWithChildren): ReactElement {
   return (
     <html lang="fr">
