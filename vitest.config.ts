9745ffc6b61fba34809dcd8fd81530e463d117ce
Bluzzi <camilledugas.pro@gmail.com>
2023-03-07 06:15:21 +0100

    vitest (config): add typewind support

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react({ babel: { plugins: ["typewind/babel"] } })],
   test: {
     globals: true,
     environment: "happy-dom",
