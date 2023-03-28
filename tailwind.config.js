7d45a076e777f3643b6befc1ec1e0cb9cf25232b
Bluzzi <camilledugas.pro@gmail.com>
2023-03-29 01:34:10 +0200

    tailwind (config): add custom border radius and box shadow

--- tailwind.config.js
+++ tailwind.config.js
@@ -44,6 +44,15 @@ module.exports = {
       "sm": { "max": "639px" }
     },
 
+    boxShadow: {
+      DEFAULT: "0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1)"
+    },
+
+    borderRadius: {
+      DEFAULT: "0.25rem",
+      "full": "9999px"
+    },
+
     extend: {
       height: {
         "0.5": "1px"
