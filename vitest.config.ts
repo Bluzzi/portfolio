51cb48575680b7a208ddf2a642b92e56055771be
Bluzzi <camilledugas.pro@gmail.com>
2023-03-29 01:47:45 +0200

    vitest (config): remove typewind support

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react({ babel: { plugins: ["typewind/babel"] } })],
+  plugins: [react()],
   test: {
     globals: true,
     environment: "happy-dom",
