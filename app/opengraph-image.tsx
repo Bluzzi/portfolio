8e59bda3070d36fdd9e579d1a9629bd1bcc52e1b
Bluzzi <camilledugas.pro@gmail.com>
2023-04-08 21:05:52 +0200

    / (page): improve og image

--- app/opengraph-image.tsx
+++ app/opengraph-image.tsx
@@ -8,12 +8,12 @@ export const contentType = "image/png";
 export default function og(): ImageResponse {
   return new ImageResponse(
     <div style={{
-      backgroundColor: "black", height: "100%", width: "100%", padding: "0 250px",
-      display: "flex", justifyContent: "center", flexDirection: "column", gap: "10px"
+      backgroundColor: "black", height: "100%", width: "100%", padding: "0 100px",
+      display: "flex", justifyContent: "center", flexDirection: "column", gap: "40px"
     }}>
-      <p style={{ fontSize: "40px", color: "white", margin: 0 }}>Camille Dugas</p>
+      <p style={{ fontSize: "60px", color: "white", margin: 0 }}>Camille Dugas</p>
 
-      <p style={{ fontSize: "25px", color: "white", margin: 0 }}>{metadata.description}</p>
+      <p style={{ fontSize: "35px", color: "white", margin: 0 }}>{metadata.description}</p>
     </div>
   );
 }
