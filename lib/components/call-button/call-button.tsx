026aa90cbd86b533289a078559380eaae010b5f9
Bluzzi <camilledugas.pro@gmail.com>
2023-09-19 18:12:28 +0200

    chore(call-button.tsx): refactor CallButton component to improve readability and maintainability
    The changes in this commit refactor the CallButton component in the lib/components/call-button/call-button.tsx file. The following changes were made:
    
    - Replaced the anonymous function declaration with an arrow function for the useEffect hook.
    - Formatted the code to improve readability by adding proper indentation and line breaks.
    - Updated the cal function calls to use object literals for better organization and readability.
    - Added additional properties to the floatingButton cal function call, including buttonColor, buttonTextColor, and buttonText.
    - Added additional properties to the ui cal function call, including styles.branding.brandColor, hideEventTypeDetails, and layout.
    
    These changes were made to improve the readability and maintainability of the CallButton component.

--- lib/components/call-button/call-button.tsx
+++ lib/components/call-button/call-button.tsx
@@ -1,13 +1,37 @@
+"use client";
+
 import type { ReactElement } from "react";
 import { getCalApi } from "@calcom/embed-react";
 import { useEffect } from "react";
 
 export function CallButton(): ReactElement {
   useEffect(() => {
-    void (async function() {
+    void (async() => {
       const cal = await getCalApi();
-      cal("floatingButton", { "calLink": "camilledugas/meeting", "hideButtonIcon": false });
-      cal("ui", { "styles": { "branding": { "brandColor": "#000000" } }, "hideEventTypeDetails": false, "layout": "month_view" });
+
+      cal(
+        "floatingButton",
+        {
+          calLink: "camilledugas/meeting",
+          hideButtonIcon: false,
+          buttonColor: "#ffffff",
+          buttonTextColor: "#000000",
+          buttonText: "Reservez un meeting"
+        }
+      );
+
+      cal(
+        "ui",
+        {
+          styles: {
+            branding: {
+              brandColor: "#000000"
+            }
+          },
+          hideEventTypeDetails: false,
+          layout: "month_view"
+        }
+      );
     })();
   }, []);
 
