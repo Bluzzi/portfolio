e56708589f0ad856d05d8644c61398a0a441e794
Bluzzi <camilledugas.pro@gmail.com>
2023-12-23 12:30:52 +0100

    fix(metadata-image.tsx): fix import statement for ImageResponse from "next/server" to "next/og" to resolve compilation error

--- lib/components/metadata-image/metadata-image.tsx
+++ lib/components/metadata-image/metadata-image.tsx
@@ -1,4 +1,4 @@
-import { ImageResponse } from "next/server";
+import { ImageResponse } from "next/og";
 import { metadata } from "#/configs/metadata";
 
 export function MetadataImage(): ImageResponse {
