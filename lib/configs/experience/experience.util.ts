f9474183f02e481160b8a9b42208258e0c79742c
Bluzzi <camilledugas.pro@gmail.com>
2024-01-18 15:14:06 +0100

    refactor(experience.util.ts): improve sorting logic in populateExperience function
    The populateExperience function in experience.util.ts has been refactored to improve the sorting logic. Previously, the function sorted the experiences based on the start date in ascending order. However, the new logic takes into account both the start and end dates to determine the order of experiences.
    
    The changes include:
    - Renaming the variable `populated` to `sorted` for clarity.
    - Adding additional checks to prioritize ongoing experiences (where the end date is null).
    - Comparing the end dates to determine which experience is more recent.
    - Comparing the start dates if the end dates are the same.
    - Keeping the original order if both start and end dates are identical.
    
    These changes ensure that the experiences are sorted correctly based on their start and end dates.

--- lib/configs/experience/experience.util.ts
+++ lib/configs/experience/experience.util.ts
@@ -3,7 +3,7 @@ import { humanizeDateDiff } from "#/utils/date/difference";
 import { dayJS } from "#/utils/day-js";
 
 export function populateExperience(experienceBase: ExperienceBase[]): Experience[] {
-  return experienceBase.map(exp => {
+  const populated = experienceBase.map(exp => {
     const dateFormat = {
       start: exp.date.start.format("DD/MM/YYYY"),
       end: exp.date.end ? exp.date.end.format("DD/MM/YYYY") : "présent"
@@ -14,5 +14,24 @@ export function populateExperience(experienceBase: ExperienceBase[]): Experience
       duration: humanizeDateDiff(exp.date.start, exp.date.end ?? dayJS()),
       dateFormat
     };
-  }).sort((a, b) => a.date.start.isBefore(b.date.start) ? 1 : -1);
+  });
+
+  populated.sort((a, b) =>  {
+    // Check if either experience is ongoing (end date is null) and prioritize it:
+    if (a.date.end === null) return -1;
+    if (b.date.end === null) return 1;
+
+    // Compare end dates to determine which is more recent:
+    if (a.date.end.isAfter(b.date.end)) return -1;
+    if (b.date.end.isAfter(a.date.end)) return 1;
+
+    // If end dates are the same, compare start dates to find the more recent one:
+    if (a.date.start.isAfter(b.date.start)) return -1;
+    if (b.date.start.isAfter(a.date.start)) return 1;
+
+    // Keep original order if both start and end dates are identical:
+    return 0;
+  });
+
+  return populated;
 }
