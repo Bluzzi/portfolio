d8d1f7a3de6d0c7bb1ccf7494a61d1bb111202b1
Bluzzi <camilledugas.pro@gmail.com>
2024-06-22 02:38:20 +0200

    style(layout.tsx): change body background color to a gray shade for improved visual aesthetics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function Layout({ children }: PropsWithChildren): ReactElement {
     <html lang="fr">
       <head />
 
-      <body className="min-h-screen scroll-smooth bg-black py-16">
+      <body className="min-h-screen scroll-smooth bg-gray-950 py-16">
         <main className="mx-auto w-1/2 xl:w-5/6 lg:w-3/4 sm:w-11/12">
           {children}
         </main>
