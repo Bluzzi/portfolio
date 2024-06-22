955191b420951c9a989457f57183e5a38b3a2704
Bluzzi <camilledugas.pro@gmail.com>
2024-06-22 02:15:38 +0200

    style(line.tsx): replace bg-gray h-0.5 with border-t border-gray-500 for line component to enhance visual consistency

--- src/lib/components/line/line.tsx
+++ src/lib/components/line/line.tsx
@@ -3,5 +3,5 @@ import type { LineProps } from "./line.type";
 import { clsx } from "clsx";
 
 export function Line({ className, ...props }: LineProps): ReactElement {
-  return <span className={clsx(className, "block bg-gray h-0.5 w-full")} {...props} />;
+  return <span className={clsx(className, "block border-t border-gray-500 w-full")} {...props} />;
 }
