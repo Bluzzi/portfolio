f4233774614e528b3f6f5d3cf97e116292d57cf7
Bluzzi <camilledugas.pro@gmail.com>
2026-03-12 11:17:30 +0100

    chore: update dependencies and devDependencies in package.json and refactor Skill type formatting in type.ts for better readability

--- src/lib/configs/experience/type.ts
+++ src/lib/configs/experience/type.ts
@@ -1,14 +1,14 @@
 import type { Dayjs } from "dayjs";
 
-export type Skill =
-  "NodeJS" | "TypeScript" | "PHP" |
-  "ReactJS" |
-  "NextJS" | "NestJS" | "ExpressJS" | "Fastify" | "GraphQL" |
-  "PostgreSQL" | "MongoDB" | "RabbitMQ" |
-  "Playwright" | "Vitest" |
-  "GCP" | "Azure" |
-  "GitHub Actions" | "GitLab CI/CD" |
-  "PowerShell";
+export type Skill
+  = "NodeJS" | "TypeScript" | "PHP"
+    | "ReactJS"
+    | "NextJS" | "NestJS" | "ExpressJS" | "Fastify" | "GraphQL"
+    | "PostgreSQL" | "MongoDB" | "RabbitMQ"
+    | "Playwright" | "Vitest"
+    | "GCP" | "Azure"
+    | "GitHub Actions" | "GitLab CI/CD"
+    | "PowerShell";
 
 export type Experience = {
   date: {
